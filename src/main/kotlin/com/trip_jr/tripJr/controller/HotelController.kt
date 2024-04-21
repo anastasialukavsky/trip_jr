498e6e67eacef6fe9437129731049a97f6b37a03
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-20 23:45:53 -0400

    add hotel controller test for post rt

---
+++ src/main/kotlin/com/trip_jr/tripJr/controller/HotelController.kt
@@ -0,0 +1,37 @@
+package com.trip_jr.tripJr.controller
+
+import com.trip_jr.tripJr.dto.hotel.HotelDTO
+import com.trip_jr.tripJr.jooq.tables.Hotel
+import com.trip_jr.tripJr.service.hotel.HotelService
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api/hotels")
+class HotelController {
+
+
+    @Autowired
+    private lateinit var hotelService: HotelService
+
+
+    @GetMapping("/")
+    fun getAllHotels(): List<Hotel> {
+        return hotelService.getAllHotels()
+    }
+
+//    @GetMapping("/{id}")
+//    fun getHotelById(@PathVariable("id") hotelId: UUID): Hotel {
+//        return hotelService.getHotelById(id)
+//    }
+
+    @PostMapping("")
+    fun createHotel(@Valid @RequestBody hotel: HotelDTO): ResponseEntity<HotelDTO> {
+        val createdHotel = hotelService.createHotel(hotel)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdHotel)
+    }
+
+}
