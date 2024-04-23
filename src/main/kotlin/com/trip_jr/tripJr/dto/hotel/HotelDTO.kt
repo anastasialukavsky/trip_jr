4e29a1978cdc390cc91dca19ac5bae60b0e282f6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-22 20:39:35 -0400

    add listOf amenities to hotel DTO

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
@@ -3,62 +3,9 @@ package com.trip_jr.tripJr.dto.hotel
 import java.util.UUID
 
 data class HotelDTO(
-    val hotelId: UUID? =null,
+    val hotelId: UUID? = null,
     val name: String = "",
     var location: LocationDTO,
-    val rates: List<RateDTO> = listOf()
+    val rates: List<RateDTO> = listOf(),
+    val amenities: List<AmenityDTO> = listOf()
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- *   fun createHotel(hotel: HotelDTO): HotelDTO? {
- *         val hotelId = hotel.hotelId ?: UUID.randomUUID()
- *
- *         // Insert into hotel table
- *         val hotelRecord = dslContext.insertInto(HOTEL)
- *             .columns(HOTEL.HOTEL_ID, HOTEL.NAME)
- *             .values(hotelId, hotel.name)
- *             .returningResult(HOTEL.HOTEL_ID, HOTEL.NAME)
- *             .fetchOne()
- *
- *         return if (hotelRecord != null) {
- *             hotelRecord.get(HOTEL.NAME)?.let {
- *                 HotelDTO(
- *                     hotelId = hotelRecord.get(HOTEL.HOTEL_ID),
- *                     name = it
- *                 )
- *             }
- *         } else {
- *             null // Return null if no hotel record is inserted
- *         }
- *     }*/
