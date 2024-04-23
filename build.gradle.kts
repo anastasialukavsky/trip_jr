a9fb5d7c70a80b98aa74422b3160a39432577d53
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-23 15:18:27 -0400

    reorganize plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     id("com.netflix.dgs.codegen") version "6.0.3"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
     id("org.jooq.jooq-codegen-gradle") version "3.19.7"
     id("application")
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "com.trip_jr"
