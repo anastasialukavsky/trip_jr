2faf8a3e0a1623bfaf5abbf0f2d22bc508c54ffa
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-24 14:11:33 -0400

    add new graphql hotel mapping controller

--- src/main/kotlin/com/trip_jr/tripJr/controller/HotelController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/HotelController.kt
@@ -1,17 +1,15 @@
 package com.trip_jr.tripJr.controller
 
 import com.trip_jr.tripJr.dto.hotel.HotelDTO
-import com.trip_jr.tripJr.jooq.tables.Hotel
 import com.trip_jr.tripJr.service.hotel.HotelService
-import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
 import java.util.*
 
-@RestController
-@RequestMapping("api/hotels")
+
+@Controller
 class HotelController {
 
 
@@ -19,21 +17,8 @@ class HotelController {
     private lateinit var hotelService: HotelService
 
 
-    @GetMapping("")
-    fun getAllHotels(): List<Hotel> {
-        return hotelService.getAllHotels()
+    @QueryMapping
+    fun hotelById(@Argument id: UUID): HotelDTO {
+        return hotelService.getHotelById(id)
     }
-
-    @GetMapping("/{id}")
-    fun getHotelById(@PathVariable("id") hotelId: UUID):  ResponseEntity<HotelDTO> {
-        val hotel =  hotelService.getHotelById(hotelId)
-        return ResponseEntity.status(HttpStatus.OK).body(hotel)
-    }
-
-    @PostMapping("")
-    fun createHotel(@Valid @RequestBody hotel: HotelDTO): ResponseEntity<HotelDTO> {
-        val createdHotel = hotelService.createHotel(hotel)
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdHotel)
-    }
-
 }
