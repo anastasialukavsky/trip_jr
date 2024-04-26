88a39147e2cfda8cf664ec4b6367cd0c2da64ed5
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 21:01:24 -0400

    add hotel controllers, add createHotel logic

--- src/main/kotlin/com/trip_jr/tripJr/controller/HotelController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/HotelController.kt
@@ -2,10 +2,15 @@ package com.trip_jr.tripJr.controller
 
 import com.trip_jr.tripJr.dto.hotel.HotelDTO
 import com.trip_jr.tripJr.service.hotel.HotelService
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
 import java.util.*
 
 
@@ -20,5 +25,12 @@ class HotelController {
     @QueryMapping
     fun hotelById(@Argument id: UUID): HotelDTO {
         return hotelService.getHotelById(id)
+    }
+
+
+    @MutationMapping
+    fun createHotel(@Argument hotel: HotelDTO): ResponseEntity<HotelDTO> {
+        val createdHotel = hotelService.createHotel(hotel)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdHotel)
     }
 }
