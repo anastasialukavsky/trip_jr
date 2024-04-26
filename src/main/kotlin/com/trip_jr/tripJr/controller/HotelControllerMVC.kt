8b2054faf760adf3e453cfb19cb1b4a18c82bf01
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 21:01:47 -0400

    old hotel controller(rest)

---
+++ src/main/kotlin/com/trip_jr/tripJr/controller/HotelControllerMVC.kt
@@ -0,0 +1,39 @@
+package com.trip_jr.tripJr.controller
+
+import com.trip_jr.tripJr.dto.hotel.HotelDTO
+import com.trip_jr.tripJr.jooq.tables.Hotel
+import com.trip_jr.tripJr.service.hotel.HotelService
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("api/hotels")
+class HotelControllerMVC {
+
+
+    @Autowired
+    private lateinit var hotelService: HotelService
+
+
+    @GetMapping("")
+    fun getAllHotels(): List<Hotel> {
+        return hotelService.getAllHotels()
+    }
+
+    @GetMapping("/{id}")
+    fun getHotelById(@PathVariable("id") hotelId: UUID):  ResponseEntity<HotelDTO> {
+        val hotel =  hotelService.getHotelById(hotelId)
+        return ResponseEntity.status(HttpStatus.OK).body(hotel)
+    }
+
+    @PostMapping("")
+    fun createHotel(@Valid @RequestBody hotel: HotelDTO): ResponseEntity<HotelDTO> {
+        val createdHotel = hotelService.createHotel(hotel)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdHotel)
+    }
+
+}
