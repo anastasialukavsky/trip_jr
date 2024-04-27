1ee2b2b98d3aed673d6f945157a0f2ca8d4d46f5
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-26 20:29:24 -0400

    change checkInDate, checkoutDate fields to LocalDate types

--- src/main/kotlin/com/trip_jr/tripJr/dto/booking/BookingDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/booking/BookingDTO.kt
@@ -1,12 +1,13 @@
 package com.trip_jr.tripJr.dto.booking
 
+import java.time.LocalDate
 import java.util.*
 
 data class BookingDTO(
     val bookingId: UUID? = null,
     val userId: UUID?,
     val hotelId: UUID?,
-    val checkInDate: Date,
-    val checkOutDate: Date,
+    val checkInDate: LocalDate,
+    val checkOutDate: LocalDate,
     val totalCost: Double,
 )
