e86b207df6f75196126fcb7619ac3c07f74f61c7
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-26 20:34:40 -0400

    add password hashing upon user creation

--- src/main/kotlin/com/trip_jr/tripJr/service/user/UserService.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/user/UserService.kt
@@ -5,10 +5,12 @@ import com.trip_jr.tripJr.dto.user.UserDTO
 //import com.trip_jr.tripJr.jooq.enums.UserRole
 import com.trip_jr.tripJr.jooq.tables.Users
 import com.trip_jr.tripJr.jooq.tables.references.USERS
+import com.trip_jr.tripJr.service.utils.PasswordUtils
 import org.slf4j.LoggerFactory
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.jooq.impl.SQLDataType
+import org.mindrot.jbcrypt.BCrypt
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.lang.reflect.Field
@@ -20,6 +22,10 @@ class UserService {
     @Autowired
     lateinit var dslContext: DSLContext
 
+
+    @Autowired
+    private lateinit var passwordUtils: PasswordUtils
+
     private val logger = LoggerFactory.getLogger(UserService::class.java)
 
 
@@ -28,39 +34,35 @@ class UserService {
     }
 
 
-    fun createUser(user: UserDTO): UserDTO? {
+    fun createUser(user: UserDTO): UserDTO {
         try {
+
+            val hashPassword = passwordUtils.hashPassword(user.passwordHash)
+            val userWithHashedPassword = user.copy(passwordHash = hashPassword)
+
             val userId = user.userId ?: generateUniqueUUID()
 
             val userRecord = dslContext.insertInto(USERS)
                 .set(USERS.USER_ID, userId)
-                .set(USERS.EMAIL, user.email)
-                .set(USERS.FIRST_NAME, user.firstName)
-                .set(USERS.LAST_NAME, user.lastName)
-                .set(USERS.PASSWORD_HASH, user.passwordHash)
+                .set(USERS.EMAIL, userWithHashedPassword.email)
+                .set(USERS.FIRST_NAME, userWithHashedPassword.firstName)
+                .set(USERS.LAST_NAME, userWithHashedPassword.lastName)
+                .set(USERS.PASSWORD_HASH, userWithHashedPassword.passwordHash)
                 .returningResult(USERS.USER_ID)
                 .fetchOne()
 
-            if (userRecord == null) {
-                throw NullPointerException("Failed to create USER")
-            }
+            userRecord ?: throw NullPointerException("User record was null.")
 
-            return userRecord.get(USERS.USER_ID)?.let {
-                UserDTO(
-                    userId = it,
-                    email = user.email,
-                    firstName = user.firstName,
-                    lastName = user.lastName,
-                    passwordHash = user.passwordHash,
-                )
-            }
+            return userWithHashedPassword.copy(userId = userRecord.get(USERS.USER_ID))
+
         } catch (e: NullPointerException) {
+            logger.error("NULL POINTER", e)
             throw e
         } catch (e: Exception) {
+            logger.error("failed to create user", e)
             throw e
         }
     }
-
 
 
 }
