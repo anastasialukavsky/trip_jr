4792b280374ba6e3d9e0bff3f0eeec5a775573f8
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-28 19:07:51 -0400

    change return for userbyId to nullable userDTO obj

--- src/main/kotlin/com/trip_jr/tripJr/service/utils/UserUtils.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/utils/UserUtils.kt
@@ -26,29 +26,39 @@ class UserUtils {
         return userRecord?.into(UserDTO::class.java)
     }
 
-    fun fetchUserBookings(id: UUID): List<BookingDTO>? {
+    fun fetchUserBookings(id: UUID): List<MutableList<BookingDTO>>? {
         try {
             val bookingsRecord = dslContext.select()
                 .from(BOOKING)
                 .where(BOOKING.USER_ID.eq(id))
-                .fetchOne()
-                ?: throw NoSuchElementException("Booking records for user with ID $id not found")
+                .fetch()
+//                ?: throw NoSuchElementException("Booking records for user with ID $id not found")
 
-            return listOf(bookingsRecord.into(BookingDTO::class.java))
+            return if (bookingsRecord.isNotEmpty) {
+                listOf(bookingsRecord.into(BookingDTO::class.java))
+
+            } else {
+                ArrayList()
+            }
         } catch (e: Exception) {
             throw e
         }
     }
 
-    fun fetchUserReviews(id: UUID): List<ReviewDTO>? {
+    fun fetchUserReviews(id: UUID): List<MutableList<ReviewDTO>>? {
         try {
             val reviewsRecord = dslContext.select()
                 .from(REVIEW)
                 .where(REVIEW.USER_ID.eq(id))
-                .fetchOne()
-                ?: throw NoSuchElementException("Booking records for user with ID $id not found")
+                .fetch()
+//                ?: throw NoSuchElementException("Booking records for user with ID $id not found")
 
-            return listOf(reviewsRecord.into(ReviewDTO::class.java))
+            return if (reviewsRecord.isNotEmpty) {
+                listOf(reviewsRecord.into(ReviewDTO::class.java))
+            } else {
+                ArrayList()
+            }
+//            return listOf(reviewsRecord.into(ReviewDTO::class.java))
         } catch (e: Exception) {
             throw e
         }
