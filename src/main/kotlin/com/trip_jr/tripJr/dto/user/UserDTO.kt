25983866db96aa31bf3d476456c7919bfdbc6590
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-28 19:06:55 -0400

    change typeof booking & reviews to MutableList

--- src/main/kotlin/com/trip_jr/tripJr/dto/user/UserDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/user/UserDTO.kt
@@ -10,8 +10,8 @@ data class UserDTO(
     val firstName: String?,
     val lastName : String?,
     val passwordHash: String = "",
-    val bookings: List<BookingDTO>? = listOf(),
-    val reviews: List<ReviewDTO>? = listOf()
+    val bookings: List<MutableList<BookingDTO>>? = listOf(),
+    val reviews: List<MutableList<ReviewDTO>>? = listOf()
 )
 
 //enum class UserRole(val literal: String) {
