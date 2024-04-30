700eccc076a38b385c90baf635d5dbbb548106d0
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-30 13:31:01 -0400

    refactor commit

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/LocationDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/LocationDTO.kt
@@ -3,11 +3,11 @@ package com.trip_jr.tripJr.dto.hotel
 import java.util.UUID
 
 data class LocationDTO(
-    val locationId: UUID? =null,
+    val locationId: UUID? = null,
     var phoneNumber: String = "",
     var address: String = "",
     var city: String = "",
-    var state:String = "",
+    var state: String = "",
     var zip: String = "",
     var latitude: Double = 0.0,
     var longitude: Double = 0.0,
