72b3b133081b34957d4fae175546bbb712a52ece
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-30 13:30:47 -0400

    add code back in

--- src/main/kotlin/com/trip_jr/tripJr/controller/booking/BookingController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/booking/BookingController.kt
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 @Controller
@@ -16,19 +14,19 @@ class BookingController {
     @Autowired
     lateinit var bookingService: BookingService
 
-//    @MutationMapping(name= "createBooking")
-//    fun createBooking(@Argument(name="booking") booking: BookingDTO): BookingDTO {
-//        val bookingId = UUID.randomUUID()
-//
-//        val newBooking = BookingDTO(
-//            bookingId = bookingId,
-//            userId = booking.userId,
-//            hotelId = booking.hotelId,
-//            checkInDate = booking.checkInDate,
-//            checkOutDate = booking.checkOutDate,
-//            totalCost = booking.totalCost
-//        )
-//
-//        return bookingService.createBooking(newBooking)
-//    }
+    @MutationMapping(name= "createBooking")
+    fun createBooking(@Argument(name="booking") booking: BookingDTO): BookingDTO {
+        val bookingId = UUID.randomUUID()
+
+        val newBooking = BookingDTO(
+            bookingId = bookingId,
+            userId = booking.userId,
+            hotelId = booking.hotelId,
+            checkInDate = booking.checkInDate,
+            checkOutDate = booking.checkOutDate,
+            totalCost = booking.totalCost
+        )
+
+        return bookingService.createBooking(newBooking)
+    }
 }
