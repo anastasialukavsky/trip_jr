26de2d5ae1f878dda6df00a15c1379abf0a73dc2
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-30 15:58:01 -0400

    add bookingsByUserId query

--- src/main/kotlin/com/trip_jr/tripJr/controller/booking/BookingController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/booking/BookingController.kt
@@ -5,6 +5,7 @@ import com.trip_jr.tripJr.service.booking.BookingService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import java.util.*
 
@@ -14,19 +15,13 @@ class BookingController {
     @Autowired
     lateinit var bookingService: BookingService
 
+    @QueryMapping(name="bookingsByUserId")
+    fun bookingsByUserId(@Argument userId: UUID): List<BookingDTO> {
+        return bookingService.getAllBookingsByUserId(userId)
+    }
+
     @MutationMapping(name= "createBooking")
     fun createBooking(@Argument(name="booking") booking: BookingDTO): BookingDTO {
-        val bookingId = UUID.randomUUID()
-
-        val newBooking = BookingDTO(
-            bookingId = bookingId,
-            userId = booking.userId,
-            hotelId = booking.hotelId,
-            checkInDate = booking.checkInDate,
-            checkOutDate = booking.checkOutDate,
-            totalCost = booking.totalCost
-        )
-
-        return bookingService.createBooking(newBooking)
+        return bookingService.createBooking(booking)
     }
 }
