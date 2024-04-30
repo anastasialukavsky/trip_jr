19cb41ef8d0e14eaf58665e7742cbc3950e63899
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-30 15:55:52 -0400

    add getAllBookings for user logic

--- src/main/kotlin/com/trip_jr/tripJr/service/booking/BookingService.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/booking/BookingService.kt
@@ -3,12 +3,15 @@ package com.trip_jr.tripJr.service.booking
 import com.trip_jr.tripJr.dto.booking.BookingDTO
 import com.trip_jr.tripJr.jooq.tables.records.BookingRecord
 import com.trip_jr.tripJr.jooq.tables.references.BOOKING
+import com.trip_jr.tripJr.service.utils.UUIDUtils
+import com.trip_jr.tripJr.service.utils.UserUtils
 import org.jooq.DSLContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 @Service
@@ -17,13 +20,29 @@ class BookingService {
     @Autowired
     lateinit var dslContext: DSLContext
 
+    @Autowired
+    lateinit var uuidUtils: UUIDUtils
+
+
+    fun getAllBookingsByUserId(userId: UUID): List<BookingDTO> {
+        try {
+            val bookingsRecord = dslContext
+                .select()
+                .from(BOOKING)
+                .where(BOOKING.USER_ID.eq(userId))
+                .fetch()
+
+            return bookingsRecord.into(BookingDTO::class.java)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 
     fun createBooking(booking: BookingDTO): BookingDTO {
         try {
-//            val totalCostBigDecimal = BigDecimal.valueOf(booking.totalCost)
-
+            val bookingId = booking.bookingId ?: uuidUtils.generateUUID()
             val bookingRecord = dslContext.insertInto(BOOKING)
-                .set(BOOKING.BOOKING_ID, booking.bookingId)
+                .set(BOOKING.BOOKING_ID,bookingId)
                 .set(BOOKING.USER_ID, booking.userId)
                 .set(BOOKING.HOTEL_ID, booking.hotelId)
                 .set(BOOKING.CHECK_IN_DATE, booking.checkInDate)
