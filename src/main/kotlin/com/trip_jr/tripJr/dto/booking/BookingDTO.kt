631724d532f4d96c9ae7303e3893fa0ffb416acf
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-04 15:52:26 -0400

    add new guestDetails fields on booking DTO

--- src/main/kotlin/com/trip_jr/tripJr/dto/booking/BookingDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/booking/BookingDTO.kt
@@ -2,13 +2,21 @@ package com.trip_jr.tripJr.dto.booking
 
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
 data class BookingDTO(
     val bookingId: UUID? = null,
     val userId: UUID?,
     val hotelId: UUID?,
+    val guestFirstName: String,
+    val guestLastName: String,
+    val numOfGuests: Int,
+    val occasion: String? = "",
+    val guestNotes: String? = "",
     val checkInDate: LocalDate,
     val checkOutDate: LocalDate,
     val totalCost: BigDecimal?,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
