03628a9dcf7ba8e8e3a4d63a1524d225909ef39d
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-04 17:36:57 -0400

    fix updateBooking query passing in new schema fields with guest info

--- src/main/kotlin/com/trip_jr/tripJr/service/booking/BookingService.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/booking/BookingService.kt
@@ -1,6 +1,7 @@
 package com.trip_jr.tripJr.service.booking
 
 import com.trip_jr.tripJr.dto.booking.BookingDTO
+import com.trip_jr.tripJr.dto.booking.UpdateBookingDTO
 import com.trip_jr.tripJr.dto.hotel.RateDTO
 import com.trip_jr.tripJr.jooq.tables.references.BOOKING
 import com.trip_jr.tripJr.jooq.tables.references.RATE
@@ -109,7 +110,12 @@ class BookingService {
         }
     }
 
-    fun updateBooking(userId: UUID, bookingId: UUID, hotelId: UUID,  booking: BookingDTO): BookingDTO {
+    fun calculateTotalCost(rateRecord: RateDTO?, durationInDays: Long): Double {
+        val rate = rateRecord?.rate ?: 0.0
+        return rate * durationInDays
+    }
+
+    fun updateBooking(userId: UUID, bookingId: UUID, hotelId: UUID, booking: UpdateBookingDTO): BookingDTO? {
         try {
 
             val bookingRecord = dslContext
@@ -117,14 +123,7 @@ class BookingService {
                 .from(BOOKING)
                 .where(BOOKING.USER_ID.eq(userId))
                 .and(BOOKING.BOOKING_ID.eq(bookingId))
-                .fetchOne()
-
-            if (bookingRecord == null) {
-                throw RuntimeException("Booking not found")
-            }
-
-            val durationInDays = ChronoUnit.DAYS.between(booking.checkInDate, booking.checkOutDate)
-
+                .fetchOne() ?: throw RuntimeException("Booking with ID $bookingId not found")
 
             val rateRecord = dslContext.select()
                 .from(RATE)
@@ -132,26 +131,77 @@ class BookingService {
                 .and(DSL.extract(booking.checkInDate, DatePart.MONTH).eq(RATE.MONTH))
                 .fetchOneInto(RateDTO::class.java)
 
+            val durationInDays = ChronoUnit.DAYS.between(booking.checkInDate, booking.checkOutDate)
+            val currentTimestamp = OffsetDateTime.now(ZoneOffset.UTC)
 
-            val totalCost = rateRecord?.rate?.times(durationInDays.toDouble()) ?: 0.0
-            val totalCostBigDecimal = totalCost.toBigDecimal()
+            val originalBookingRecord: BookingDTO? =
+                bookingRecord.get(BOOKING.GUEST_FIRST_NAME)?.let {
+                    bookingRecord.get(BOOKING.NUM_OF_GUESTS)?.let { it1 ->
+                        bookingRecord.get(BOOKING.CHECK_OUT_DATE)?.let { it2 ->
+                            bookingRecord.get(BOOKING.CREATED_AT)?.toLocalDateTime()?.let { it3 ->
+                                BookingDTO(
+                                    bookingId = bookingId,
+                                    userId = userId,
+                                    hotelId = hotelId,
+                                    guestFirstName = it,
+                                    guestLastName = bookingRecord.get(BOOKING.GUEST_LAST_NAME)!!,
+                                    numOfGuests = it1,
+                                    occasion = bookingRecord.get(BOOKING.OCCASION),
+                                    guestNotes = bookingRecord.get(BOOKING.GUEST_NOTES),
+                                    checkInDate = bookingRecord.get(BOOKING.CHECK_IN_DATE)!!,
+                                    checkOutDate = it2,
+                                    totalCost = bookingRecord.get(BOOKING.TOTAL_COST),
+                                    createdAt = it3,
+                                    updatedAt = currentTimestamp.toLocalDateTime()
+                                )
+                            }
+                        }
+                    }
+                }
 
-            val updatedBookingRecord = dslContext.update(BOOKING)
-                .set(BOOKING.CHECK_IN_DATE, booking.checkInDate)
-                .set(BOOKING.CHECK_OUT_DATE, booking.checkOutDate)
-                .set(BOOKING.TOTAL_COST, totalCostBigDecimal)
+            val originalCheckInDate = originalBookingRecord?.checkInDate
+            val originalCheckOutDate = originalBookingRecord?.checkOutDate
+
+
+            val updatedBookingRecord = originalBookingRecord?.copy(
+                guestFirstName = booking.guestFirstName ?: originalBookingRecord.guestFirstName,
+                guestLastName = booking.guestLastName ?: originalBookingRecord.guestLastName,
+                numOfGuests = booking.numOfGuests ?: originalBookingRecord.numOfGuests,
+                occasion = booking.occasion ?: originalBookingRecord.occasion,
+                guestNotes = booking.guestNotes ?: originalBookingRecord.guestNotes,
+                checkInDate = booking.checkInDate ?: originalBookingRecord.checkInDate,
+                checkOutDate = booking.checkOutDate ?: originalBookingRecord.checkOutDate,
+                totalCost = if (booking.checkInDate != null && booking.checkOutDate != null &&
+                    (booking.checkInDate != originalCheckInDate || booking.checkOutDate != originalCheckOutDate)
+                ) {
+                    calculateTotalCost(rateRecord, durationInDays).toBigDecimal()
+                } else {
+                    originalBookingRecord?.totalCost
+                },
+
+                createdAt = originalBookingRecord.createdAt,
+                updatedAt = originalBookingRecord.updatedAt
+            )
+
+
+            val updateQuery = dslContext.update(BOOKING)
+                .set(BOOKING.GUEST_FIRST_NAME, updatedBookingRecord?.guestFirstName)
+                .set(BOOKING.GUEST_LAST_NAME, updatedBookingRecord?.guestLastName)
+                .set(BOOKING.NUM_OF_GUESTS, updatedBookingRecord?.numOfGuests)
+                .set(BOOKING.OCCASION, updatedBookingRecord?.occasion)
+                .set(BOOKING.GUEST_NOTES, updatedBookingRecord?.guestNotes)
+                .set(BOOKING.CHECK_IN_DATE, updatedBookingRecord?.checkInDate)
+                .set(BOOKING.CHECK_OUT_DATE, updatedBookingRecord?.checkOutDate)
+                .set(BOOKING.TOTAL_COST, updatedBookingRecord?.totalCost)
+                .set(BOOKING.UPDATED_AT, currentTimestamp)
+                .set(BOOKING.UPDATED_AT, currentTimestamp)
                 .where(BOOKING.USER_ID.eq(userId))
                 .and(BOOKING.BOOKING_ID.eq(bookingId))
                 .execute()
 
-            if (updatedBookingRecord == 1) {
-                val updatedRecord = dslContext
-                    .selectFrom(BOOKING)
-                    .where(BOOKING.USER_ID.eq(userId))
-                    .and(BOOKING.BOOKING_ID.eq(bookingId))
-                    .fetchOneInto(BookingDTO::class.java)
 
-                return updatedRecord ?: throw RuntimeException("Failed to fetch updated booking")
+            if (updateQuery == 1) {
+                return updatedBookingRecord
             } else {
                 throw RuntimeException("Failed to update booking")
             }
@@ -159,6 +209,5 @@ class BookingService {
             throw e
         }
     }
-
 
 }
