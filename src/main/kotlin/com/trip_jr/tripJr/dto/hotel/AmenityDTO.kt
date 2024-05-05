5d39c5271307a45686f16ec43649fc0dc14bb99f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-05 19:39:00 -0400

    add timestamps

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/AmenityDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/AmenityDTO.kt
@@ -1,9 +1,12 @@
 package com.trip_jr.tripJr.dto.hotel
 
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class AmenityDTO(
     val amenityId: UUID? = null,
     var amenityName: String = "",
-    val hotelId: UUID?
+    val hotelId: UUID?,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
