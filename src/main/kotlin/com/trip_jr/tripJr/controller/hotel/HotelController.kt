3a8b3d65c39c0e1bfefaffe194d57ef482510434
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-05 14:05:59 -0400

    add mutation mapping to update hotel

--- src/main/kotlin/com/trip_jr/tripJr/controller/hotel/HotelController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/hotel/HotelController.kt
@@ -1,6 +1,7 @@
 package com.trip_jr.tripJr.controller.hotel
 
 import com.trip_jr.tripJr.dto.hotel.HotelDTO
+import com.trip_jr.tripJr.dto.hotel.updateDTOs.UpdateHotelDTO
 import com.trip_jr.tripJr.service.hotel.HotelService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
@@ -20,20 +21,29 @@ class HotelController {
     private lateinit var hotelService: HotelService
 
 
-    @QueryMapping(name="hotels")
-    fun hotels() : List<HotelDTO> {
+    @QueryMapping(name = "hotels")
+    fun hotels(): List<HotelDTO> {
         return hotelService.getAllHotels()
     }
 
     @QueryMapping
-    fun hotelById(@Argument id: UUID): HotelDTO {
+    fun hotelById(@Argument id: UUID): HotelDTO? {
         return hotelService.getHotelById(id)
     }
 
 
-    @MutationMapping(name="createHotel")
-    fun createHotel(@Argument(name="hotel") hotel: HotelDTO): ResponseEntity<HotelDTO> {
+    @MutationMapping(name = "createHotel")
+    fun createHotel(@Argument(name = "hotel") hotel: HotelDTO): ResponseEntity<HotelDTO> {
         val createdHotel = hotelService.createHotel(hotel)
         return ResponseEntity.status(HttpStatus.CREATED).body(createdHotel)
+    }
+
+    @MutationMapping(name = "updateHotel")
+    fun updateHotel(
+        @Argument(name = "id") id: UUID,
+        @Argument(name = "hotel") hotel: UpdateHotelDTO
+    )
+    : HotelDTO? {
+        return hotelService.updateHotel(id, hotel)
     }
 }
