86a9670f5cb6abd10277811099d4d8981a9201ca
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-05 14:05:03 -0400

    remove comments

--- src/main/kotlin/com/trip_jr/tripJr/db.connect.kt
+++ src/main/kotlin/com/trip_jr/tripJr/db.connect.kt
@@ -8,11 +8,10 @@ fun main() {
     val password = "postgrespass"
     val url = "jdbc:postgresql://localhost:5432/tripjr"
 
-    // Connection is the only JDBC resource that we need
-    // PreparedStatement and ResultSet are handled by jOOQ, internally
+
     try {
         DriverManager.getConnection(url, userName, password).use { conn ->
-            // Your code for database operations here
+
         }
     } catch (e: Exception) {
         e.printStackTrace()
