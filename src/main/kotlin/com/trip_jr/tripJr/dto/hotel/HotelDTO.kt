cf9063cf03cc061e1f15444a93e5b9dd77d05e5e
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-04 23:02:58 -0400

    add numRoom, desc, and timestamps

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
@@ -2,14 +2,19 @@ package com.trip_jr.tripJr.dto.hotel
 
 import com.trip_jr.tripJr.dto.booking.BookingDTO
 import com.trip_jr.tripJr.dto.review.ReviewDTO
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class HotelDTO(
     val hotelId: UUID? = null,
     val name: String = "",
+    val numOfRooms: Int? = 1,
+    val description: String? = "",
     var location: LocationDTO,
     val rates: List<RateDTO> = listOf(),
     val amenities: List<AmenityDTO> = listOf(),
     val reviews: List<ReviewDTO> = listOf(),
-    val bookings: List<BookingDTO> = listOf()
+    val bookings: List<BookingDTO> = listOf(),
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
