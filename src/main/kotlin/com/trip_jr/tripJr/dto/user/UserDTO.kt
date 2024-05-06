7612ab863beb172f72bf12fd7307f207b8bec691
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-05 20:41:22 -0400

    add timestamps

--- src/main/kotlin/com/trip_jr/tripJr/dto/user/UserDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/user/UserDTO.kt
@@ -2,6 +2,7 @@ package com.trip_jr.tripJr.dto.user
 
 import com.trip_jr.tripJr.dto.booking.BookingDTO
 import com.trip_jr.tripJr.dto.review.ReviewDTO
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class UserDTO(
@@ -11,7 +12,9 @@ data class UserDTO(
     val lastName : String?,
     val passwordHash: String = "",
     val bookings: MutableList<BookingDTO>? = mutableListOf(),
-    val reviews: MutableList<ReviewDTO>? = mutableListOf()
+    val reviews: MutableList<ReviewDTO>? = mutableListOf(),
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
 
 //enum class UserRole(val literal: String) {
