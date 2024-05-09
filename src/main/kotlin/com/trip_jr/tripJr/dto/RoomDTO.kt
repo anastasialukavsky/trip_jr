80fd4a8677950ca29a71bc85e41f907fb26d116b
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-08 22:24:03 -0400

    add roomDTO

--- src/main/kotlin/com/trip_jr/tripJr/dto/RoomDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/RoomDTO.kt
@@ -1,5 +1,6 @@
 package com.trip_jr.tripJr.dto
 
+import com.trip_jr.tripJr.dto.hotel.RateDTO
 import com.trip_jr.tripJr.jooq.enums.BedType
 import com.trip_jr.tripJr.jooq.enums.RoomStatus
 import com.trip_jr.tripJr.jooq.enums.RoomType
@@ -7,17 +8,38 @@ import java.time.LocalDateTime
 import java.util.*
 
 data class RoomDTO(
-    val id: UUID,
+    val roomId: UUID? = null,
     val hotelId: UUID,
+    val rate: RateDTO? = null,
+    val roomNumber: Int,
     val roomType: RoomType,
     val roomStatus: RoomStatus,
     val bedType: BedType,
     val maximumOccupancy: Int,
     val description: String,
     val floor: Int,
-    val availability: String,
+    val availability: Boolean,
     val lastCleaned: LocalDateTime = LocalDateTime.now(),
     val createdAt: LocalDateTime = LocalDateTime.now(),
     val updatedAt: LocalDateTime = LocalDateTime.now(),
-
     )
+
+//enum class RoomType {
+//    SINGLE,
+//    DOUBLE,
+//    SUIT
+//}
+//
+//enum class RoomStatus {
+//    AVAILABLE,
+//    OCCUPIED,
+//    RESERVED,
+//    MAINTENANCE,
+//}
+//
+//enum class BedType {
+//    SINGLE,
+//    DOUBLE,
+//    QUEEN,
+//    KING
+//}
