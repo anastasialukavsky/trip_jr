0d67ab4d0cf4c1ee44f7c9f2ba1b7936ce595428
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-10 19:31:45 -0400

    add updateRoomDTO

---
+++ src/main/kotlin/com/trip_jr/tripJr/controller/hotel/room/UpdateRoomDTO.kt
@@ -0,0 +1,22 @@
+package com.trip_jr.tripJr.controller.hotel.room
+
+import com.trip_jr.tripJr.dto.hotel.RateDTO
+import com.trip_jr.tripJr.jooq.enums.BedType
+import com.trip_jr.tripJr.jooq.enums.RoomStatus
+import com.trip_jr.tripJr.jooq.enums.RoomType
+import java.time.LocalDateTime
+import java.util.*
+
+data class UpdateRoomDTO(
+val roomNumber: Int?,
+val roomType: RoomType?,
+val roomStatus: RoomStatus?,
+val bedType: BedType?,
+val maximumOccupancy: Int?,
+val description: String?,
+val floor: Int?,
+val availability: Boolean?,
+val lastCleaned: LocalDateTime? = LocalDateTime.now(),
+val updatedAt: LocalDateTime? = LocalDateTime.now(),
+
+)
