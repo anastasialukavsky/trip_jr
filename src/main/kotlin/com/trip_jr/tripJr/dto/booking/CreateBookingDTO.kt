9c47e8f3557ce4c63fa47a7b38f89f3e864856c2
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-14 20:21:39 -0400

    add type CreateBookingDTO

---
+++ src/main/kotlin/com/trip_jr/tripJr/dto/booking/CreateBookingDTO.kt
@@ -0,0 +1,23 @@
+package com.trip_jr.tripJr.dto.booking
+
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+data class CreateBookingDTO(
+    val bookingId: UUID? = null,
+    val userId: UUID?,
+    val hotelId: UUID?,
+    val guestFirstName: String,
+    val guestLastName: String,
+    val numOfGuests: Int,
+    val occasion: String? = "",
+    val guestNotes: String? = "",
+    val checkInDate: LocalDate,
+    val checkOutDate: LocalDate,
+    val totalCost: BigDecimal?,
+    val roomDetails: BookingRoomDetailsDTO,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+)
