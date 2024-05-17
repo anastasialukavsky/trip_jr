4a42feb02761d5e7ef7f61dadcd2081fd96cb0a9
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-16 22:19:31 -0400

    add jjwt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.mindrot:jbcrypt:0.4")
-
+//    implementation ("org.springframework.boot:spring-boot-starter-security")
+//    implementation ("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     //TODO configure liquibase
 //    implementation("org.liquibase:liquibase-core")
     implementation("org.postgresql:postgresql:42.6.2")
