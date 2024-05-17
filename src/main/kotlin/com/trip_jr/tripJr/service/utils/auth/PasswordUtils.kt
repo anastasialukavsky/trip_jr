7ff251f16b702cbd185cafaf391e6a87869da68f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-16 22:20:08 -0400

    add check password util func

--- src/main/kotlin/com/trip_jr/tripJr/service/utils/auth/PasswordUtils.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/utils/auth/PasswordUtils.kt
@@ -1,7 +1,6 @@
-package com.trip_jr.tripJr.service.utils
+package com.trip_jr.tripJr.service.utils.auth
 
 import org.mindrot.jbcrypt.BCrypt
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 
 @Component
@@ -9,5 +8,9 @@ class PasswordUtils {
 
     fun hashPassword(password: String): String {
         return BCrypt.hashpw(password, BCrypt.gensalt(10))
+    }
+
+    fun checkPassword(plainPassword: String, passwordHash: String) : Boolean {
+        return BCrypt.checkpw(plainPassword, passwordHash)
     }
 }
