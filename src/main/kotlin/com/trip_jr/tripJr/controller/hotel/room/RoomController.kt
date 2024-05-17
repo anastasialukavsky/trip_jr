2a8855a47814f5e457cc2e5ae6ee6f4361fe29a7
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-16 20:35:01 -0400

    import path refactor

--- src/main/kotlin/com/trip_jr/tripJr/controller/hotel/room/RoomController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/hotel/room/RoomController.kt
@@ -1,14 +1,12 @@
 package com.trip_jr.tripJr.controller.hotel.room
 
-import com.trip_jr.tripJr.dto.RoomDTO
-import com.trip_jr.tripJr.dto.hotel.RateDTO
+import com.trip_jr.tripJr.dto.hotel.RoomDTO
 import com.trip_jr.tripJr.service.hotel.room.RoomService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
-import reactor.core.publisher.Flux
 import java.util.*
 
 
@@ -36,7 +34,7 @@ class RoomController {
     @MutationMapping(name="createRoom")
     fun createRoom(
         @Argument(name="hotelId") hotelId: UUID,
-        @Argument(name="room") room:  RoomDTO,
+        @Argument(name="room") room: RoomDTO,
 //        @Argument(name="rate") rate: RateDTO,
         ): RoomDTO? {
         return roomService.createRoom(hotelId, room)
