531ffb45e0295edf18797d3879899892fa4bf6c9
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-16 22:19:52 -0400

    add generatye jwt util func

---
+++ src/main/kotlin/com/trip_jr/tripJr/service/utils/jwt/JwtUtils.kt
@@ -0,0 +1,23 @@
+package com.trip_jr.tripJr.service.utils.jwt
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.Cipher.SECRET_KEY
+
+@Component
+class JwtUtils {
+    companion object {
+        private const val EXPIRATION_TIME = 86400000 //1 day
+        private const val SECRET_KEY = "secret_key"
+    }
+
+    fun generateToken(email: String): String {
+        return Jwts.builder()
+            .setSubject(email)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(Keys.hmacShaKeyFor(SECRET_KEY.toByteArray()))
+            .compact()
+    }
+}
