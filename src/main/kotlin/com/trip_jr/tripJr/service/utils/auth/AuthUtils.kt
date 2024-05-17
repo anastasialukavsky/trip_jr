17095e06b6c96027d554bea9aa9c60eb1f07252f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-16 22:19:05 -0400

    add fetchUserPassword& email util funcs

--- src/main/kotlin/com/trip_jr/tripJr/service/utils/auth/AuthUtils.kt
+++ src/main/kotlin/com/trip_jr/tripJr/service/utils/auth/AuthUtils.kt
@@ -1,7 +1,40 @@
 package com.trip_jr.tripJr.service.utils.auth
 
+import com.trip_jr.tripJr.jooq.tables.references.USERS
+import com.trip_jr.tripJr.service.user.UserService
+import org.jooq.DSLContext
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class AuthUtils {
+
+    @Autowired
+    lateinit var dslContext: DSLContext
+
+    fun fetchUserEmail(email: String): Boolean {
+        try {
+            val userEmail = dslContext.select()
+                .from(USERS)
+                .where(USERS.EMAIL.eq(email))
+                .fetchOne() ?: throw RuntimeException("Email $email not found")
+
+            return userEmail != null
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    fun fetchUserPassword(passwordHash: String): String {
+        try {
+            return dslContext.select()
+                .from(USERS)
+                .where(USERS.PASSWORD_HASH.eq(passwordHash))
+                .fetchOne(USERS.PASSWORD_HASH) ?: throw RuntimeException("Password not found")
+
+
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }
