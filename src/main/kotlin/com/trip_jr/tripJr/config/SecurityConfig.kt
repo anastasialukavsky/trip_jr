ccb5633804fc0166c86d8f6c57ba3366717a1430
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-31 15:31:20 -0400

    add basic security config

--- src/main/kotlin/com/trip_jr/tripJr/config/SecurityConfig.kt
+++ src/main/kotlin/com/trip_jr/tripJr/config/SecurityConfig.kt
@@ -2,36 +2,30 @@ package com.trip_jr.tripJr.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.server.SecurityWebFilterChain
+
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeHttpRequests(
-                Customizer { authorize: AuthorizationManagerRequestMatcherRegistry ->
-                    authorize
-                        .requestMatchers("/blog/**").permitAll()
-                        .anyRequest().authenticated()
-                }
-            )
-            .formLogin { formLogin: FormLoginConfigurer<HttpSecurity?> ->
-                formLogin
-                    .loginPage("/login")
-                    .permitAll()
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/graphql").permitAll()
+                    .anyRequest().authenticated()
             }
-            .rememberMe(Customizer.withDefaults())
+            .httpBasic { }
 
         return http.build()
-    }
 
+    }
 }
