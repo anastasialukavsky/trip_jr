2bfc2eaa9eff32477945b9dbb7dda5232f1400a6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-03 22:54:30 -0400

    add update user role query

--- src/main/kotlin/com/trip_jr/tripJr/repository/user/UserRepository.kt
+++ src/main/kotlin/com/trip_jr/tripJr/repository/user/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.trip_jr.tripJr.repository.user
 
 import com.trip_jr.tripJr.dto.user.UserDTO
+import com.trip_jr.tripJr.jooq.enums.UserRole
 import com.trip_jr.tripJr.jooq.tables.Users
 import com.trip_jr.tripJr.jooq.tables.references.USERS
 import org.jooq.DSLContext
@@ -17,11 +18,12 @@ class UserRepository(private val dslContext: DSLContext) {
             .fetchOneInto(UserDTO::class.java)
     }
 
-//    fun updateUserRole(userId: UUID, role: Role) {
-//        dslContext.update(USERS)
-//            .set(USERS.ROLE, role.name)
-//            .where(USERS.USER_ID.eq(userId))
-//            .execute()
-//    }
+    fun updateUserRole(userId: UUID, role: UserRole) {
+        dslContext.update(USERS)
+            .set(USERS.ROLE, role)
+            .where(USERS.USER_ID.eq(userId))
+            .execute()
+    }
+
 
 }
