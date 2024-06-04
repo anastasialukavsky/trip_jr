aa5c42ba91355312f8e6172dd09b88b26767e9d2
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-03 22:51:16 -0400

    add timestamps to HotelClaimDTO

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelClaimDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelClaimDTO.kt
@@ -1,11 +1,14 @@
 package com.trip_jr.tripJr.dto.hotel
 
 import com.trip_jr.tripJr.jooq.enums.ClaimStatus
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class HotelClaimDTO(
     val claimId: UUID,
     val hotelId: UUID? = null,
     val userId: UUID? =null,
-    val status: ClaimStatus
+    val status: ClaimStatus,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
