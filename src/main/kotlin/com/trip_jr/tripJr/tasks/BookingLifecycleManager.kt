84a5bad64ae72add7ea664209d484d3d5a709213
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-05 19:48:28 -0400

    add logging to track the progress, change task to run each day 11am ETC

--- src/main/kotlin/com/trip_jr/tripJr/tasks/BookingLifecycleManager.kt
+++ src/main/kotlin/com/trip_jr/tripJr/tasks/BookingLifecycleManager.kt
@@ -3,6 +3,8 @@ package com.trip_jr.tripJr.tasks
 import com.trip_jr.tripJr.jooq.enums.RoomStatus
 import com.trip_jr.tripJr.repository.hotel.BookingRepository
 import com.trip_jr.tripJr.repository.hotel.room.RoomRepository
+import org.slf4j.LoggerFactory
+//import org.hibernate.validator.internal.util.logging.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDate
@@ -16,24 +18,36 @@ class BookingLifecycleManager(
     private val roomRepository: RoomRepository
 ) {
 
+    private val logger = LoggerFactory.getLogger(BookingLifecycleManager::class.java)
 
-    @Scheduled(fixedRate=6000) //checks db for expired bookings every 24hrs at 11am EST every day
-//    @Scheduled(cron = "0 0 11 * * ?", zone = "America/New_York") //checks db for expired bookings every 24hrs at 11am EST every day
+//    @Scheduled(fixedRate = 6000) //checks db for expired bookings every 24hrs at 11am EST every day
+    @Scheduled(cron = "0 0 11 * * ?", zone = "America/New_York") //checks db for expired bookings every 24hrs at 11am EST every day
     fun removeExpiredBookings() {
+        logger.info("Running scheduled task to check for expired bookings")
         val currentDate = LocalDate.now()
 
         val expiredBookings = bookingRepository.findBookingsByCheckOutDate(currentDate)
+        if (expiredBookings != null && expiredBookings.isNotEmpty()) {
+
+            logger.info("FETCHED ${expiredBookings?.size} bookings")
+        } else {
+            logger.warn("No expired bookings found")
+        }
 
         if (expiredBookings != null) {
             for (booking in expiredBookings) {
-                val roomId = booking.roomDetails?.roomId ?: continue
+                logger.info("Processing Booking $booking")
+                val roomId = booking.roomDetails?.roomId
+                if (roomId == null) {
+                    logger.warn("Room $roomId does not exist")
+                    continue
+                }
 
                 roomRepository.updateRoomStatus(roomId, RoomStatus.Maintenance)
+                logger.info("Booking $roomId has been expired, MAINTENANCE is scheduled")
                 scheduleMaintenanceEnd(roomId)
             }
         }
-
-
     }
 
 
@@ -42,6 +56,7 @@ class BookingLifecycleManager(
 
         executorService.schedule({
             roomRepository.updateRoomStatus(roomId, RoomStatus.Vacant)
+            logger.info("Updated room {} to Vacant", roomId)
         }, 1, TimeUnit.HOURS)
     }
 
