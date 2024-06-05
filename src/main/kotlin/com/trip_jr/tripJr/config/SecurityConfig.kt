7a5d285371cc64aef42778a8906ffbf44351e145
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-04 20:07:22 -0400

    comment out security config for now

--- src/main/kotlin/com/trip_jr/tripJr/config/SecurityConfig.kt
+++ src/main/kotlin/com/trip_jr/tripJr/config/SecurityConfig.kt
@@ -1,31 +1,31 @@
-package com.trip_jr.tripJr.config
-
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.server.SecurityWebFilterChain
-
-
-@Configuration
-@EnableWebSecurity
-class SecurityConfig {
-
-    @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf { csrf -> csrf.disable() }
-            .authorizeHttpRequests { auth ->
-                auth.requestMatchers("/graphql").permitAll()
-                    .anyRequest().authenticated()
-            }
-            .httpBasic { }
-
-        return http.build()
-
-    }
-}
+//package com.trip_jr.tripJr.config
+//
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.http.HttpMethod
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+//import org.springframework.security.config.web.server.ServerHttpSecurity
+//import org.springframework.security.web.SecurityFilterChain
+//import org.springframework.security.web.server.SecurityWebFilterChain
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//class SecurityConfig {
+//
+//    @Bean
+//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+//        http
+//            .csrf { csrf -> csrf.disable() }
+//            .authorizeHttpRequests { auth ->
+//                auth.requestMatchers("/graphql").permitAll()
+//                    .anyRequest().authenticated()
+//            }
+//            .httpBasic { }
+//
+//        return http.build()
+//
+//    }
+//}
