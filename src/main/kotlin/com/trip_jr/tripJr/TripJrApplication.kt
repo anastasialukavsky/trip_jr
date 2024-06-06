ddf116941ae7f35aa5795b43114863cb5dff8003
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-06 17:53:46 -0400

    add enable config props flag

--- src/main/kotlin/com/trip_jr/tripJr/TripJrApplication.kt
+++ src/main/kotlin/com/trip_jr/tripJr/TripJrApplication.kt
@@ -1,9 +1,11 @@
 package com.trip_jr.tripJr
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties
 class TripJrApplication
 
 fun main(args: Array<String>) {
