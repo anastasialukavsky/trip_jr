2716af442aa56cc4bcd25aef5fe3fe9d1a5f16a0
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-06 17:51:45 -0400

    add cloud aws config

---
+++ src/main/kotlin/com/trip_jr/tripJr/config/AWSConfig.kt
@@ -0,0 +1,35 @@
+package com.trip_jr.tripJr.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.S3Configuration
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import java.net.URI
+
+
+@Configuration
+class AWSConfig {
+
+    @Bean
+    fun s3Client(): S3Client {
+        return S3Client.builder()
+            .region(Region.US_EAST_1)
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accesskey", "secretkey")))
+            .endpointOverride(URI.create("http://localhost:4566"))
+            .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+            .build()
+    }
+
+    @Bean
+    fun s3Presigner(): S3Presigner {
+        return S3Presigner.builder()
+            .region(Region.of("us-east-1"))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accesskey", "secretkey")))
+            .endpointOverride(URI.create("http://localhost:4566"))
+            .build()
+    }
+}
