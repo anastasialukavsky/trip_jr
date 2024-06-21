27b7a07e42ca17f36d57b673e84c5d2dbef6f53e
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-21 18:05:57 -0400

    change region syntax

--- src/main/kotlin/com/trip_jr/tripJr/config/AWSConfig.kt
+++ src/main/kotlin/com/trip_jr/tripJr/config/AWSConfig.kt
@@ -17,7 +17,7 @@ class AWSConfig {
     @Bean
     fun s3Client(): S3Client {
         return S3Client.builder()
-            .region(Region.of("us-east-1"))
+            .region(Region.US_EAST_1)
             .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accesskey", "secretkey")))
             .endpointOverride(URI.create("http://localhost:4566"))
             .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
@@ -27,7 +27,7 @@ class AWSConfig {
     @Bean
     fun s3Presigner(): S3Presigner {
         return S3Presigner.builder()
-            .region(Region.of("us-east-1"))
+            .region(Region.US_EAST_1)
             .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accesskey", "secretkey")))
             .endpointOverride(URI.create("http://localhost:4566"))
             .build()
