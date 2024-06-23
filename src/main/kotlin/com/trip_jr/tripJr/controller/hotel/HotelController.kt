9479d7038df7739bc53824303b80dfe12b9d933a
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-23 16:26:11 -0400

    add hotelSearchQuery

--- src/main/kotlin/com/trip_jr/tripJr/controller/hotel/HotelController.kt
+++ src/main/kotlin/com/trip_jr/tripJr/controller/hotel/HotelController.kt
@@ -1,6 +1,7 @@
 package com.trip_jr.tripJr.controller.hotel
 
 import com.trip_jr.tripJr.dto.hotel.HotelDTO
+import com.trip_jr.tripJr.dto.hotel.HotelSearchDTO
 import com.trip_jr.tripJr.dto.hotel.updateDTOs.UpdateHotelDTO
 import com.trip_jr.tripJr.service.aws.S3Service
 import com.trip_jr.tripJr.service.hotel.HotelService
@@ -8,10 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDate
 import java.util.*
 
 
@@ -40,10 +39,10 @@ class HotelController {
     @MutationMapping(name = "createHotel")
     fun createHotel(
         @Argument(name = "hotel") hotel: HotelDTO,
-        @Argument(name="hotelImageURLs") hotelImageURLs: List<MultipartFile>
+//        @Argument(name="hotelImageURLs") hotelImageURLs: List<MultipartFile>
         ): HotelDTO? {
-        val imageURLs = s3Service.uploadImages(hotelImageURLs)
-        return hotelService.createHotel(hotel, imageURLs)
+//        val imageURLs = s3Service.uploadImages(hotelImageURLs)
+        return hotelService.createHotel(hotel)
     }
 
     @MutationMapping(name = "updateHotel")
@@ -58,5 +57,14 @@ class HotelController {
     @MutationMapping(name = "deleteHotel")
     fun deleteHotel(@Argument id: UUID): Boolean {
         return hotelService.deleteHotel(id)
+    }
+
+    @QueryMapping(name = "hotelSearch")
+    fun hotelSearch(
+        @Argument(name="location") location: String,
+        @Argument(name="checkInDate") checkInDate: LocalDate,
+        @Argument(name="checkOutDate") checkOutDate: LocalDate,
+    ): List<HotelSearchDTO> {
+        return hotelService.hotelSearch(location, checkInDate, checkOutDate)
     }
 }
