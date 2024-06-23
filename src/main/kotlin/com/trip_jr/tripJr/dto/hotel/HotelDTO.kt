01a9be8867d3f89589b7eefc4d406ed6b3195bd0
anastasia lukavsky <stacylukavsky@gmail.com>
2024-06-23 16:26:26 -0400

    comment our hotelImages field for now

--- src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
+++ src/main/kotlin/com/trip_jr/tripJr/dto/hotel/HotelDTO.kt
@@ -3,14 +3,14 @@ package com.trip_jr.tripJr.dto.hotel
 import com.trip_jr.tripJr.dto.booking.BookingDTO
 import com.trip_jr.tripJr.dto.review.ReviewDTO
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
 data class HotelDTO(
     val hotelId: UUID? = null,
     val name: String = "",
     val numOfRooms: Int? = 1,
     val description: String? = "",
-    val hotelImageURLs: List<String>? = listOf(),
+//    val hotelImageURLs: List<String>? = listOf(),
     var location: LocationDTO,
     val amenities: List<AmenityDTO> = listOf(),
     val reviews: List<ReviewDTO> = listOf(),
